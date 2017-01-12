b3e4cde0d8faf15810cdea96e0573e1c041e8757
王子华 <wangzihua@we-te.com>
2017-01-12 11:54:57 +0800

    log pre

--- view/log/index.blade.php
+++ view/log/index.blade.php
@@ -31,7 +31,7 @@
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
-                        {{$data}}
+                        <pre>{{$data}}</pre>
                     </div>
                 </div>
 
